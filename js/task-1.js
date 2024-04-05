70a6af79391a3820ca8216da02a3241c28d064a4
vladickjerseyclub <pakizhj@gmail.com>
2024-04-05 09:13:21 +0200

    first-check

---
+++ js/task-1.js
@@ -0,0 +1,28 @@
+// function isEnoughCapacity(products, containerSize) {
+//   const productsValues = Object.values(products);
+//   let totalValues = 0;
+//   for (const product of productsValues) {
+//     totalValues += product;
+//   }
+//   if (totalValues <= containerSize) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function isEnoughCapacity(products, containerSize) {
+  let totalValues = 0;
+  for (const product of Object.values(products)) {
+    totalValues += product;
+  }
+  return totalValues <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
